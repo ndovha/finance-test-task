5f503dd8eaa0667f52a2f27ab72143526710ec85
Nataliia Dovha <natadnepr027@gmail.com>
2022-01-17 16:18:00 +0200

    styles

---
+++ client/src/modules/finance/finance.component.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect} from 'react';
+import styles from './index.module.scss'
+import socketIOClient from 'socket.io-client';
+
+const SERVER_URL = 'http://localhost:4000';
+export function Finance () {
+     
+    const [quotes, setQuotes] = useState([]);
+
+    useEffect(() => {
+        const socket = socketIOClient(SERVER_URL);
+        socket.emit('start');
+        socket.on('ticker', data => {           
+            setQuotes(data);
+        });
+      }, []);
+
+        return (
+            <div className={styles.wrapper}>
+                <ul className={styles.wrapper}>
+                    {quotes.map((quote, index) => {
+                        return (
+
+                            <li key={index} className={styles.list}>
+                                <div className={styles.list__item}>
+                                {/* <div className={styles.list__ticker}>{quote.ticker}</div> */}
+                                {/* <div className={styles.list__ticker}>{quote.ticker}</div> */}
+                                {/* <div className={styles.list__name}>{quote.ticker}</div> */}
+                                {/* <div className={styles.list__price}>{quote.price}</div> */}
+                                {/* <div className={styles.list__change}>{quote.change}</div> */}
+                                {/* <div className={styles.list__change_percent}>{quote.change_percent}</div>                       */}
+                                </div>   
+                            </li>
+                        )
+                    })
+                   
+                    }
+                </ul>
+            </div>
+        )
+
+    
+}
---
+++ client/src/pages/finance.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Finance from 'modules/finance';
+
+export default function FinancePage() {
+    return (
+        <div>
+            <Finance />
+        </div>
+    );
+}
